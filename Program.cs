c1ff7c629dc00df5675b562c96a664dc83398d38
TeemuHe <teemuhoo@luukku.com>
2019-01-08 15:12:14 +0200

    done

--- Program.cs
+++ Program.cs
@@ -6,13 +6,16 @@ namespace CatProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is a cat program");
+            Console.WriteLine("Tämä on kissa ohjelma.");
 
-            Cat myFirstCat = new Cat("Pilli",1);
-            //myFirstCat.weight = 5;
-            //Console.WriteLine($"Cats weight is changed {myFirstCat.weight}");
+            Cat myFirstCat = new Cat("Pilli", 1);
+            myFirstCat.IncreaseWeight(1);
+            myFirstCat.PrintWeight();
+
+            Console.WriteLine("---------------------------");
 
             Cat mySecondCat = new Cat("Pulla", 3);
+            mySecondCat.PrintWeight();
 
             Console.Write("Press any key to continue...");
             Console.ReadKey();
