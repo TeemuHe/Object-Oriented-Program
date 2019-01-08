f026313055ce2b6892441b554ff88a2b6d090c51
TeemuHe <teemuhoo@luukku.com>
2019-01-08 14:11:52 +0200

    .

---
+++ CatProgram/Cat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatProgram
+{
+    class Cat
+    {
+        public string name;
+        public int weight;
+
+        public Cat()
+        {
+            this.weight = 1;
+            //Console.WriteLine($"Cats start weight is {this.weight}");
+        }
+
+        public Cat(string name, int weightValue)
+        {
+            this.name = name;
+            this.weight = weightValue;
+            Console.WriteLine($"Created cat {this.name}");
+        }
+
+        public Cat(int weightValue)
+        {
+            this.weight = weightValue;
+            //Console.WriteLine($"Cats weight is {this.weight}");
+        }
+
+        public void printWeight()
+        {
+            Console.WriteLine($"Cats weight is {this.weight}");
+        }
+    }
+}
---
+++ CatProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CatProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a cat program");
+
+            Cat myFirstCat = new Cat("Pilli",1);
+            //myFirstCat.weight = 5;
+            //Console.WriteLine($"Cats weight is changed {myFirstCat.weight}");
+
+            Cat mySecondCat = new Cat("Pulla", 3);
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
