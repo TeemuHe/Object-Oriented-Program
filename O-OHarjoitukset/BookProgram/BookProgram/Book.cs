137ef7e165544bad288359916fff1f2330d435ab
TeemuHe <teemuhoo@luukku.com>
2019-01-09 09:36:16 +0200

    done

---
+++ O-OHarjoitukset/BookProgram/BookProgram/Book.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookProgram
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public string id;
+        public double price;
+
+        public Book(string title)
+        {
+            this.title = title;
+            Console.WriteLine($"Nimi: {this.title}.");
+        }
+        public void Author(string author)
+        {
+            this.author = author;
+            Console.WriteLine($"Kirjailija: {this.author}.");
+        }
+        public void Id(string id)
+        {
+            this.id = id;
+            Console.WriteLine($"Sarjanumero: {this.id}.");
+        }
+        public void Price(double price)
+        {
+            this.price = price;
+            Console.WriteLine($"Hinta: {this.price}.");
+        }
+        /*public string PrintInfo()
+        {
+            Console.WriteLine($"Nimi: {title}\nKirjoittaja: {author}\nSarjanumero{id}\nHinta: {price}");
+        }*/
+        public string CompareBook(Book book)
+        {
+            string text = "";
+            //Console.WriteLine(this.price);
+            if (this.price > book.price)
+            {
+                Console.WriteLine($"{this.title} on kalliimpi kuin {book.title}.");
+            }
+            else if(this.price == book.price)
+            {
+                Console.WriteLine($"{this.title} on yhtä kallis kuin {book.title}.");
+            }
+            else
+            {
+                Console.WriteLine($"{this.title} on halvempi kuin {book.title}.");
+            }
+            return text;
+        }
+    }
+}
---
+++ O-OHarjoitukset/BookProgram/BookProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BookProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjat:");
+
+            Book firstBook = new Book("Aapinen");
+            firstBook.Author("Mikael Agricola");
+            firstBook.Id("22222");
+            firstBook.Price(15.89);
+            Console.WriteLine("--------------------");
+
+            Book secondBook = new Book("Peppi Pitkätossu");
+            secondBook.Author("Astrid Lindgren");
+            secondBook.Id("12345");
+            secondBook.Price(9.99);
+            Console.WriteLine("--------------------");
+
+            firstBook.CompareBook(secondBook);
+        }
+    }
+}
