acf399e625dbc44acb500f60217b25e5a1b0bb05
TeemuHe <teemuhoo@luukku.com>
2019-01-11 14:33:32 +0200

    done and pushed

---
+++ O-OHarjoitukset/CarProgram/CarProgram/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarProgram
+{
+    class Car
+    {
+        public string brand;
+        public double speed;
+        public double extraSpeed;
+
+        public void AskData()
+        {
+            Console.Write("Syötä merkki: ");
+            brand = Console.ReadLine();
+            Console.Write("Nopeus: ");
+            speed = int.Parse(Console.ReadLine());
+            Console.Clear();
+        }
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Auton merkki: {this.brand}\nAuton nopeus: {this.speed}");
+            Console.Clear();
+        }
+        public void Accelerate()
+        {
+            do
+            {
+                Console.Write($"Paljonko nopeutta lisätään {this.brand}lle ? ");
+                extraSpeed = int.Parse(Console.ReadLine());
+                if(extraSpeed < 0)
+                {
+                    Console.WriteLine("Syötä positiivinen luku!");
+                }
+                this.speed += extraSpeed;
+            } while (extraSpeed <= 0);
+            Console.WriteLine($"{this.brand} kiihdyttää ja kulkee lopulta {this.speed}km/h.");
+        }
+        public void Brake()
+        {
+            Console.WriteLine($"Nyt {this.brand} hidastaa ja kulkee {this.speed * 0.9}km/h.");
+        }
+    }
+}
---
+++ O-OHarjoitukset/CarProgram/CarProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CarProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Autot:");
+
+            Car car = new Car();
+            car.AskData();
+            Car newCar = new Car();
+            newCar.AskData();
+
+            car.ShowCarInfo();
+            newCar.ShowCarInfo();
+
+            car.Accelerate();
+            newCar.Accelerate();
+
+            car.Brake();
+            newCar.Brake();
+        }
+    }
+}
