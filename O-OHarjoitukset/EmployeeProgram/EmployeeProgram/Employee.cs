fc2c673e6c69445fd6a23f3ef8b6ac200eddce10
TeemuHe <teemuhoo@luukku.com>
2019-01-17 11:44:46 +0200

    CompareSalary done

---
+++ O-OHarjoitukset/EmployeeProgram/EmployeeProgram/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeProgram
+{
+    class Employee
+    {
+        public string name;
+        public int id;
+        public string position ;
+        public double salary;
+        private static int counter = 1;
+
+        public Employee(string name, int id, string position, double salary)
+        {
+            this.name = name;
+            this.id = counter++; ;
+            this.position = position;
+            this.salary = salary;
+        }
+
+        public void PrintEmployeeInfo(string name, string id, string position, double salary)
+        {
+            Console.WriteLine($"Työntekijä: {this.name}\nId: {this.id}\nTyötehtävä: {this.position}\nPalkka: {this.salary}");
+            Console.WriteLine("-------------------------");
+        }
+
+        public string CompareSalary(Employee firstEmployee, Employee secondEmployee)
+        {
+            string text = "";
+            if(this.salary > firstEmployee.salary && firstEmployee.salary > secondEmployee.salary)
+            {
+                Console.WriteLine($"{this.name} saa enemmän palkkaa kuin {firstEmployee.name} ja {firstEmployee.name} saa enemmän kuin {secondEmployee.name}.");
+            }
+            else if (this.salary < firstEmployee.salary && firstEmployee.salary > secondEmployee.salary && this.salary < secondEmployee.salary)
+            {
+                Console.WriteLine($"{firstEmployee.name} saa enemmän palkkaa kuin {secondEmployee.name} ja {secondEmployee.name} saa enemmän kuin {this.name}.");
+            }
+            else if (this.salary < firstEmployee.salary && firstEmployee.salary > secondEmployee.salary && this.salary > secondEmployee.salary)
+            {
+                Console.WriteLine($"{firstEmployee.name} saa enemmän palkkaa kuin {this.name} ja {this.name} saa enemmän kuin {secondEmployee.name}.");
+            }
+            else if (this.salary > firstEmployee.salary && firstEmployee.salary < secondEmployee.salary && this.salary > secondEmployee.salary)
+            {
+                Console.WriteLine($"{firstEmployee.name} saa enemmän palkkaa kuin {this.name} ja {this.name} saa enemmän kuin {secondEmployee.name}.");
+            }
+            else if (this.salary > firstEmployee.salary && firstEmployee.salary < secondEmployee.salary && this.salary < secondEmployee.salary)
+            {
+                Console.WriteLine($"{secondEmployee.name} saa enemmän palkkaa kuin {this.name} ja {this.name} saa enemmän kuin {firstEmployee.name}.");
+            }
+            else if (this.salary < firstEmployee.salary && firstEmployee.salary < secondEmployee.salary && this.salary < secondEmployee.salary)
+            {
+                Console.WriteLine($"{secondEmployee.name} saa enemmän palkkaa kuin {firstEmployee.name} ja {firstEmployee.name} saa enemmän kuin {this.name}.");
+            }
+            return text;
+        }
+    }
+}
---
+++ O-OHarjoitukset/EmployeeProgram/EmployeeProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmployeeProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Työntekijäluokka");
+
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee("Matti", 0, "Opettaja", 2500);
+            employees[1] = new Employee("Juha", 0, "Poliisi", 3000);
+            employees[2] = new Employee("Hannu", 0, "Myyjä", 2000);
+
+            for (int i = 0; i < 2; i++)
+            {
+                employees[i].PrintEmployeeInfo("nimi", "id", "tehtävä", 0);
+            }
+
+            employees[0].CompareSalary(employees[1],employees[2]);
+        }
+    }
+}
