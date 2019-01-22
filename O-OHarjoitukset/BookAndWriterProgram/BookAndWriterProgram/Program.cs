bf15b2f23e9c159ab3093484fbb096ed7b6c9f4a
TeemuHe <teemuhoo@luukku.com>
2019-01-22 11:14:16 +0200

    Initialized

---
+++ O-OHarjoitukset/BookAndWriterProgram/BookAndWriterProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookAndWriterProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
